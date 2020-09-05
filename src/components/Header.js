16a1e403b6cec98ead470dfc823ed4762dd3d3a8
Anirudh Banerjee <imanirudh.dev@gmail.com>
2020-09-05 21:41:19 +0530

    Liner Gradient

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Header(props) {
     return (
-        <div style={{color:'purple',margin:50,textAlign:"center",fontSize:22}} >
+        <div className={{color:"white",margin:50,textAlign:"center",fontSize:22}} >
             <h1>Todo List</h1>
         </div>
     )
 }
+
 
 export default Header
